0c911ad7781684c53af0fe7022cbe99a7b8ba162
jamesddestefano-ui <james.d.destefano@gmail.com>
2026-09-10 14:27:47 -0400

    Seed Sparta in test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
 import pytest
-from sqlalchemy import event
 from sqlalchemy.orm import Session
 
 from ffdb.db import make_engine
 from ffdb.models import Base
 from ffdb.seed import seed_mongo
+from ffdb.sparta_seed import seed_sparta
+
 
 @pytest.fixture()
 def session(tmp_path):
     engine = make_engine(tmp_path / "test.db")
     Base.metadata.create_all(engine)
     with Session(engine) as s:
-        with s.begin(): seed_mongo(s)
+        with s.begin():
+            seed_mongo(s)
+            seed_sparta(s)
         yield s
-
